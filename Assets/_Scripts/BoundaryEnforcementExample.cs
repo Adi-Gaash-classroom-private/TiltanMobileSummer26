513176ab8f9eddd708b0f052920854f958e260d7
AG <adigaash@gmail.com>
2026-09-03 15:50:12 +0300

    inspector staff

---
+++ Assets/_Scripts/BoundaryEnforcementExample.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace TiltanMobileSummer2026
+{
+    public class BoundaryEnforcementExample : MonoBehaviour
+    {
+        [Range(0.1f, 10f)]
+        [SerializeField] private float speed = 5f;
+
+        [TextArea(3, 10)]
+        [SerializeField] private string itemDescription = "Enter description here...";
+    }
+}
---
+++ Assets/_Scripts/VisualOrganizationExample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace TiltanMobileSummer2026
+{
+    public class VisualOrganizationExample : MonoBehaviour
+    {
+        [Header("Combat Parameters")]
+        [Tooltip("Controls bullet spread angle in degrees.")]
+        [SerializeField] private float bulletSpread = 5f;
+
+        [Space(10)]
+
+        [Header("Movement Parameters")]
+        [Tooltip("Defines maximum movement speed.")]
+        [SerializeField] private float moveSpeed = 10f;
+    }
+}
