fc46256a3db12bf32e02114bd3653baa23dec712
AG <adigaash@gmail.com>
2026-09-03 15:34:31 +0300

    floatvar1

---
+++ Assets/_Scripts/Generic/FloatVariable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace TiltanMobileSummer2026.Generic
+{
+    [CreateAssetMenu(fileName = "FloatVariable", menuName = "Variables/Float Variable")]
+    public class FloatVariable : ScriptableObject
+    {
+        [SerializeField] private float value;
+
+        public float Value
+        {
+            get => value;
+            set => this.value = value;
+        }
+    }
+}
