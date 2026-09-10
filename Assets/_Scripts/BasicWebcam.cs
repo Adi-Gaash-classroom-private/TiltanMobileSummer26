98016227a6b064f584721fd597a3091c0f6a3270
AG <adigaash@gmail.com>
2026-09-10 14:57:11 +0300

    init

---
+++ Assets/_Scripts/BasicWebcam.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace TiltanMobileSummer2026
+{
+    public class BasicWebcam: MonoBehaviour
+    {
+        private WebCamTexture webcamTexture;
+
+        void Start()
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+
+            if (devices.Length == 0)
+            {
+                Debug.LogError("No webcam devices found.");
+                return;
+            }
+
+            // Initialize the first available webcam at 1280x720, 30 FPS
+            webcamTexture = new WebCamTexture(devices[0].name, 1280, 720, 30);
+
+            Renderer camRenderer = GetComponent<Renderer>();
+            if (camRenderer != null)
+            {
+                camRenderer.material.mainTexture = webcamTexture;
+            }
+
+            webcamTexture.Play();
+        }
+
+        void OnDisable()
+        {
+            if (webcamTexture != null && webcamTexture.isPlaying)
+            {
+                webcamTexture.Stop();
+            }
+        }
+    }
+}
